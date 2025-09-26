9288b90fa133754c4c3d6235dfca77d6333daf15
Manwa LapTops <manwalaptops@192.168.1.100>
2025-09-26 17:21:57 +0400

    Successful build and deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./Images/logo.png"
 
